698c831e217a6489760ea85f5c8e7d5ef5dc4617
Jalagon <A1ag0nJus1in@gmail.com>
2024-04-20 13:35:41 -0700

    Build

--- Assets/_CraftGame/Scripts/CommonBackend/SoundBootstrap.cs
+++ Assets/_CraftGame/Scripts/CommonBackend/SoundBootstrap.cs
@@ -10,11 +10,6 @@ public class SoundBootstrap : MonoBehaviour
 	private IEnumerator Start()
 	{
 		yield return new WaitForSeconds(1f);
-		
-<<<<<<< HEAD
 		MMSoundManagerSoundPlayEvent.Trigger(_bgMusic, MMSoundManager.MMSoundManagerTracks.Music, default, loop: true, volume: 0.5f);
-=======
-		MMSoundManagerSoundPlayEvent.Trigger(_bgMusic, MMSoundManager.MMSoundManagerTracks.Music, default, loop: true, volume: 0.65f);
->>>>>>> Justin
 	}
 }
